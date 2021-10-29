4abc987b2691130a04c79639b8e783db34c9ae68
jayzone91 <50213036+jayzone91@users.noreply.github.com>
2021-10-29 09:48:30 +0200

    Start project

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,17 @@
 module.exports = function(eleventyConfig) {
 
+    // Watchtargets
     eleventyConfig.addWatchTarget('./src/scss/');
 
+    // Passthrough Copies
+
+    // Shortcodes
+    eleventyConfig.addShortcode("year", () => {
+        let tmp = new Date().getFullYear();
+        return tmp.toString();
+    });
+
+    // Input- / Output-Directories
     return {
         dir: {
             input: "src",
